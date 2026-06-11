31b34f2dda6653fcc813a516567b18122316b848
Abhinav Arora <aroraabhinav2007@gmail.com>
2026-06-11 19:14:30 +0530

    Docs, packaging, and cross-platform BLAS support

--- setup.py
+++ setup.py
@@ -1,14 +1,21 @@
 from setuptools import setup, find_packages
 
 try:
+    import sys
     from pybind11.setup_helpers import Pybind11Extension, build_ext
+
+    if sys.platform == "darwin":
+        blas_link = ["-framework", "Accelerate"]
+    else:
+        blas_link = ["-lopenblas"]
+
     ext_modules = [
         Pybind11Extension(
             "bare_metal_ml._cpp",
             sources=["bare_metal_ml/cpp/bindings.cpp"],
             include_dirs=["bare_metal_ml/cpp"],
             extra_compile_args=["-O3", "-std=c++17"],
-            extra_link_args=["-framework", "Accelerate"],
+            extra_link_args=blas_link,
         ),
     ]
     cmdclass = {"build_ext": build_ext}
